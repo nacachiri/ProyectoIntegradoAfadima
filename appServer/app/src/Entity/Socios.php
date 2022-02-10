bd36597729609d5dd54c7002f1adfab466180ef7
nacachiri <nachocalomarde@gmail.com>
2022-02-10 20:48:51 +0100

    instalado el jwt

--- appServer/app/src/Entity/Socios.php
+++ appServer/app/src/Entity/Socios.php
@@ -2,26 +2,31 @@
 
 namespace App\Entity;
 
-use App\Repository\SociosRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\Noticias;
+use App\Entity\Servicios;
+use App\Entity\Numerarios;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\SociosRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: SociosRepository::class)]
-class Socios
+class Socios implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
     private $email;
 
     #[ORM\Column(type: 'string', length: 255)]
     private $password;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'json', length: 255)]
     private $rol;
 
     #[ORM\Column(type: 'string', length: 50)]
@@ -81,12 +86,19 @@ class Socios
         return $this;
     }
 
-    public function getRol(): ?string
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
     {
-        return $this->rol;
+        $roles = $this->rol;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
-    public function setRol(string $rol): self
+    public function setRol(array $rol): self
     {
         $this->rol = $rol;
 
@@ -212,4 +224,24 @@ class Socios
 
         return $this;
     }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
 }

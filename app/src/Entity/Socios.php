1717926013cd4de67d93d8c87b3281a1f14247d3
Nacachiri <nachocalomarde@gmail.com>
2022-02-14 09:25:17 +0100

    null in numerario

--- app/src/Entity/Socios.php
+++ app/src/Entity/Socios.php
@@ -42,7 +42,7 @@ class Socios implements UserInterface, PasswordAuthenticatedUserInterface
     private $phone;
 
     #[ORM\OneToOne(targetEntity: Numerarios::class, cascade: ['persist', 'remove'])]
-    #[ORM\JoinColumn(name:'numerarioId', referencedColumnName:'id', nullable: false)]
+    #[ORM\JoinColumn(name:'numerarioId', referencedColumnName:'id', nullable: true)]
     private $numerarioId;
 
     #[ORM\OneToMany(mappedBy: 'socioId', targetEntity: Servicios::class)]

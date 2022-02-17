068acacf4e7556c509ebd49f4b001ca1321dd9e7
nacachiri <nachocalomarde@gmail.com>
2022-02-17 01:04:36 +0100

    Error en productos

--- app/src/Repository/ProductosRepository.php
+++ app/src/Repository/ProductosRepository.php
@@ -77,7 +77,7 @@ class ProductosRepository extends ServiceEntityRepository
 
     public function edit(Productos $producto, $imagen, $name, $description, $numerarioId): Productos
     {
-        empty($title) ? true : $producto->setName($name);
+        empty($name) ? true : $producto->setName($name);
         empty($description) ? true : $producto->setDescription($description);
         empty($socioId) ? true : $producto->setNumerarioId($this->manager->getRepository(Numerarios::class)->findOneBy(['id' => $numerarioId]));
         empty($imagen) ? true : move_uploaded_file($imagen->getRealPath(), 'Imagenes/productos/'.$producto->getImgUrl());

7973fad621b8ca19512fb87910438abbd6e4d205
nacachiri <nachocalomarde@gmail.com>
2022-02-17 14:57:03 +0100

    inteto

--- app/public/index.php
+++ app/public/index.php
@@ -4,9 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-echo ini_set( "post_max_size", "500M");
-echo ini_set( "upload_max_filesize", "500M");
-
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };

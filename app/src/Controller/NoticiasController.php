e992e507b0fed317351c399b4de6bfea52d867a4
nacachiri <nachocalomarde@gmail.com>
2022-02-17 13:10:53 +0100

    prueba saber donde esta img

--- app/src/Controller/NoticiasController.php
+++ app/src/Controller/NoticiasController.php
@@ -42,7 +42,7 @@ class NoticiasController extends AbstractController
 
         $imgUrl = uniqid().'.'.$imagen->getClientOriginalExtension();
 
-        move_uploaded_file($imagen->getRealPath(), 'Imagenes/noticias/'.$imgUrl);
+        // move_uploaded_file($imagen->getRealPath(), 'Imagenes/noticias/'.$imgUrl);
 
         if (empty($title) || empty($description) || empty($socioId)) {
             throw new NotFoundHttpException('Los parametros no son correctos');
@@ -52,7 +52,7 @@ class NoticiasController extends AbstractController
 
         return new JsonResponse([
             'status' => 'Noticia Creada correctamente',
-            'realPath' => $imagen
+            'realPath' => $imagen->getRealPath()
         ], Response::HTTP_CREATED);
     }
 

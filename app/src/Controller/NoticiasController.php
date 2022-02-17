85bc876357247b4361881cd43af8316e977c6c28
nacachiri <nachocalomarde@gmail.com>
2022-02-17 13:31:15 +0100

    img dd

--- app/src/Controller/NoticiasController.php
+++ app/src/Controller/NoticiasController.php
@@ -42,7 +42,7 @@ class NoticiasController extends AbstractController
 
         $imgUrl = uniqid().'.'.$imagen->getClientOriginalExtension();
 
-        dd($imagen->getError());
+        dd($imagen);
 
         move_uploaded_file($imagen->getRealPath(), 'Imagenes/noticias/'.$imgUrl);
 

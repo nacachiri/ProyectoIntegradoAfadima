02289388a88ac849055d88ca1c54c814f4abddae
nacachiri <nachocalomarde@gmail.com>
2022-02-17 13:29:03 +0100

    prueba request files

--- app/src/Controller/NoticiasController.php
+++ app/src/Controller/NoticiasController.php
@@ -42,7 +42,7 @@ class NoticiasController extends AbstractController
 
         $imgUrl = uniqid().'.'.$imagen->getClientOriginalExtension();
 
-        dd($title);
+        dd($request->files);
 
         move_uploaded_file($imagen->getRealPath(), 'Imagenes/noticias/'.$imgUrl);
 

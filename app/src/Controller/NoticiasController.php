45a11e516332f680078b8a63fe54eaa3528ae719
nacachiri <nachocalomarde@gmail.com>
2022-02-17 12:26:10 +0100

    var dums noticias

--- app/src/Controller/NoticiasController.php
+++ app/src/Controller/NoticiasController.php
@@ -40,7 +40,14 @@ class NoticiasController extends AbstractController
         $description = $request->request->get('description');
         $socioId = $request->request->get('socioId');
 
+        var_dump($imagen);
+
         $imgUrl = uniqid().'.'.$imagen->getClientOriginalExtension();
+
+        var_dump($imgUrl);
+
+        var_dump($imagen->getClientOriginalExtension());
+        var_dump($imagen->getRealPath());
 
         move_uploaded_file($imagen->getRealPath(), 'Imagenes/noticias/'.$imgUrl);
 

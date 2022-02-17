d3cc7bba02fd112c7fe55449591967d63e202fa1
nacachiri <nachocalomarde@gmail.com>
2022-02-17 13:29:59 +0100

    get eeror

--- app/src/Controller/NoticiasController.php
+++ app/src/Controller/NoticiasController.php
@@ -42,7 +42,7 @@ class NoticiasController extends AbstractController
 
         $imgUrl = uniqid().'.'.$imagen->getClientOriginalExtension();
 
-        dd($request->files);
+        dd($imagen->error);
 
         move_uploaded_file($imagen->getRealPath(), 'Imagenes/noticias/'.$imgUrl);
 

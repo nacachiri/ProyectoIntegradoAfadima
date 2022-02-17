1bbe509a0e470f03636210260ec8e66df8db8834
nacachiri <nachocalomarde@gmail.com>
2022-02-17 14:56:23 +0100

    intento 3

--- app/public/index.php
+++ app/public/index.php
@@ -2,10 +2,10 @@
 
 use App\Kernel;
 
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
 echo ini_set( "post_max_size", "500M");
 echo ini_set( "upload_max_filesize", "500M");
-
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);

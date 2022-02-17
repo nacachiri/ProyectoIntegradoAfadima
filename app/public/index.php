2a93ea2d305e0c1e157be9ae6367ca84b356d224
nacachiri <nachocalomarde@gmail.com>
2022-02-17 14:29:30 +0100

    prueba iniset

--- app/public/index.php
+++ app/public/index.php
@@ -2,6 +2,9 @@
 
 use App\Kernel;
 
+ini_set( "post_max_size", "500M");
+ini_set( "upload_max_filesize", "500M");
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
